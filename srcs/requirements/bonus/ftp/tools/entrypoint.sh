f92a2487658a2bcbc8ebc8de10fb73f0aa0d03de
Ja1m3st <jaimesanchezguinea@gmail.com>
2025-05-05 13:20:16 +0200

    125

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -1,40 +1,28 @@
 #!/bin/sh
 
-# Instalar paquetes necesarios si no existen
-if [ -f /etc/vsftpd.configured ]; then
-    exec /usr/sbin/vsftpd /etc/vsftpd.conf
-    exit 0
+if id "$FTP_USER" &>/dev/null; then
+    echo "El usuario $FTP_USER ya existe, omitiendo creación..."
+else
+    adduser -D -h "/home/$FTP_USER" -s /bin/false "$FTP_USER"
+    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 fi
 
-# Validar variables
-if [ -z "$FTP_USER" ] || [ -z "$FTP_PASSWORD" ]; then
-    echo "ERROR: FTP_USER o FTP_PASSWORD no definidas" >&2
-    exit 1
-fi
-
-# Configurar usuario
-adduser -D -h "/home/$FTP_USER" -s /bin/false "$FTP_USER"
-echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-
-# Configurar directorio
 mkdir -p "/home/$FTP_USER/ftp"
 chown -R "$FTP_USER:$FTP_USER" "/home/$FTP_USER/ftp"
 chmod -R 755 "/home/$FTP_USER/ftp"
 
-# Configuración final de vsftpd
 {
-    echo "$seccomp_sandbox" >> /etc/vsftpd.conf
-    echo "$listen" >> /etc/vsftpd.conf
-    echo "$pasv_min_port"
-    echo "$pasv_max_port"
-    echo "$anonymous_enable"
-    echo "$write_enable"
-    echo "$local_enable"
-    echo "$chroot_local_user"
-    echo "$allow_writeable_chroot"
-} >> /etc/vsftpd.conf
+    echo "seccomp_sandbox=$seccomp_sandbox"
+    echo "listen=$listen"
+    echo "pasv_min_port=$pasv_min_port"
+    echo "pasv_max_port=$pasv_max_port"
+    echo "anonymous_enable=$anonymous_enable"
+    echo "write_enable=$write_enable"
+    echo "local_enable=$local_enable"
+    echo "chroot_local_user=$chroot_local_user"
+    echo "allow_writeable_chroot=$allow_writeable_chroot"
+} > /etc/vsftpd.conf
 
 echo "$FTP_USER" > /etc/vsftpd.userlist
 
-# Ejecutar en primer plano (importante para Docker)
 exec /usr/sbin/vsftpd /etc/vsftpd.conf
